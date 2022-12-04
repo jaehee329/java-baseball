4be9e8a9ed7780a460658dd90a45d86ed5310bbb
jaehee329 <jaehee329@yonsei.ac.kr>
2022-12-04 15:46:26 +0900

    feat(BaseballState): 정답 상태 문자열 반환 기능 구현

--- src/test/java/baseball/BaseballStateTest.java
+++ src/test/java/baseball/BaseballStateTest.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class BaseballStateTest {
 
@@ -32,5 +37,21 @@ class BaseballStateTest {
 	void isCorrectTest() {
 		String input = "123";
 		Assertions.assertTrue(baseballState.isCorrect(input));
+	}
+
+	@ParameterizedTest
+	@MethodSource("inputAnswerSource")
+	void getScoreStringTest(String input, String expected) {
+		org.assertj.core.api.Assertions.assertThat(baseballState.getScoreString(input)).isEqualTo(expected);
+	}
+
+	private static Stream<Arguments> inputAnswerSource() { // argument source method
+		return Stream.of(
+			Arguments.of("123", "3스트라이크"),
+			Arguments.of("231", "3볼"),
+			Arguments.of("456", "낫싱"),
+			Arguments.of("391", "2볼"),
+			Arguments.of("713", "1볼 1스트라이크")
+		);
 	}
 }
