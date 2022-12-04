c3778b83fe4ae57628b35b772d233115f20a524d
jaehee329 <jaehee329@yonsei.ac.kr>
2022-12-04 15:59:34 +0900

    feat(OutputView): 점수 출력 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
 	public void printStartMessage() {
 		System.out.println("숫자 야구 게임을 시작합니다.");
 	}
+
+	public void printScore(String score) {
+		System.out.println(score);
+	}
 }
